852dc44b84b08af3b527cf994e6f39eee626c1e0
Tategoto Azarasi <2724167997@qq.com>
2023-08-03 18:34:57 +0800

    IP多播选路
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- sys/netinet/ip_mroute.h
+++ sys/netinet/ip_mroute.h
@@ -1,4 +1,6 @@
-/*
+/**
+ * @file
+ * @copyright
  * Copyright (c) 1989 Stephen Deering.
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
@@ -52,122 +54,122 @@
 /*
  * DVMRP-specific setsockopt commands.
  */
-#define	DVMRP_INIT	100
-#define	DVMRP_DONE	101
-#define	DVMRP_ADD_VIF	102
-#define	DVMRP_DEL_VIF	103
-#define	DVMRP_ADD_LGRP	104
-#define	DVMRP_DEL_LGRP	105
-#define	DVMRP_ADD_MRT	106
-#define	DVMRP_DEL_MRT	107
+#define DVMRP_INIT 100    ///< mrouted开始
+#define DVMRP_DONE 101    ///< mrouted被关闭
+#define DVMRP_ADD_VIF 102 ///< 增加虚拟接口
+#define DVMRP_DEL_VIF 103 ///< 删除虚拟接口
+#define DVMRP_ADD_LGRP 104///< 为某个接口增加多播组入口
+#define DVMRP_DEL_LGRP 105///< 为某个接口删除多播组入口
+#define DVMRP_ADD_MRT 106 ///< 增加多播路由
+#define DVMRP_DEL_MRT 107 ///< 删除多播路由
 
 
 /*
  * Types and macros for handling bitmaps with one bit per virtual interface.
  */
-#define	MAXVIFS 32
+#define MAXVIFS 32
 typedef u_long vifbitmap_t;
-typedef u_short vifi_t;		/* type of a vif index */
+typedef u_short vifi_t;///< type of a vif index
 
-#define	VIFM_SET(n, m)		((m) |= (1 << (n)))
-#define	VIFM_CLR(n, m)		((m) &= ~(1 << (n)))
-#define	VIFM_ISSET(n, m)	((m) & (1 << (n)))
-#define	VIFM_CLRALL(m)		((m) = 0x00000000)
-#define	VIFM_COPY(mfrom, mto)	((mto) = (mfrom))
-#define	VIFM_SAME(m1, m2)	((m1) == (m2))
+#define VIFM_SET(n, m) ((m) |= (1 << (n)))
+#define VIFM_CLR(n, m) ((m) &= ~(1 << (n)))
+#define VIFM_ISSET(n, m) ((m) & (1 << (n)))
+#define VIFM_CLRALL(m) ((m) = 0x00000000)
+#define VIFM_COPY(mfrom, mto) ((mto) = (mfrom))
+#define VIFM_SAME(m1, m2) ((m1) == (m2))
 
 
-/*
+/**
  * Agument structure for DVMRP_ADD_VIF.
  * (DVMRP_DEL_VIF takes a single vifi_t argument.)
  */
 struct vifctl {
-	vifi_t	    vifc_vifi;	    	/* the index of the vif to be added */
-	u_char	    vifc_flags;     	/* VIFF_ flags defined below */
-	u_char	    vifc_threshold; 	/* min ttl required to forward on vif */
-	struct	in_addr vifc_lcl_addr;	/* local interface address */
-	struct	in_addr vifc_rmt_addr;	/* remote address (tunnels only) */
+	vifi_t vifc_vifi;            ///< the index of the vif to be added
+	u_char vifc_flags;           ///< VIFF_ flags defined below
+	u_char vifc_threshold;       ///< min ttl required to forward on vif
+	struct in_addr vifc_lcl_addr;///< local interface address
+	struct in_addr vifc_rmt_addr;///< remote address (tunnels only)
 };
 
-#define	VIFF_TUNNEL	0x1		/* vif represents a tunnel end-point */
+#define VIFF_TUNNEL 0x1///< vif represents a tunnel end-point
 
 
-/*
+/**
  * Argument structure for DVMRP_ADD_LGRP and DVMRP_DEL_LGRP.
  */
 struct lgrplctl {
-	vifi_t	lgc_vifi;
-	struct	in_addr lgc_gaddr;
+	vifi_t lgc_vifi;
+	struct in_addr lgc_gaddr;
 };
 
 
-/*
+/**
  * Argument structure for DVMRP_ADD_MRT.
  * (DVMRP_DEL_MRT takes a single struct in_addr argument, containing origin.)
  */
 struct mrtctl {
-	struct	in_addr mrtc_origin;	/* subnet origin of multicasts */
-	struct	in_addr mrtc_originmask; /* subnet mask for origin */
-	vifi_t	mrtc_parent;    	/* incoming vif */
-	vifbitmap_t mrtc_children;	/* outgoing children vifs */
-	vifbitmap_t mrtc_leaves;	/* subset of outgoing children vifs */
+	struct in_addr mrtc_origin;    ///< subnet origin of multicasts
+	struct in_addr mrtc_originmask;///< subnet mask for origin
+	vifi_t mrtc_parent;            ///< incoming vif
+	vifbitmap_t mrtc_children;     ///< outgoing children vifs
+	vifbitmap_t mrtc_leaves;       ///< subset of outgoing children vifs
 };
 
 
 #ifdef KERNEL
 
-/*
+/**
  * The kernel's virtual-interface structure.
  */
 struct vif {
-	u_char	v_flags;		/* VIFF_ flags defined above */
-	u_char	v_threshold;		/* min ttl required to forward on vif */
-	struct	in_addr v_lcl_addr;	/* local interface address */
-	struct	in_addr v_rmt_addr;	/* remote address (tunnels only) */
-	struct	ifnet  *v_ifp;		/* pointer to interface */
-	struct	in_addr *v_lcl_grps;	/* list of local grps (phyints only) */
-	int	v_lcl_grps_max;		/* malloc'ed number of v_lcl_grps */
-	int	v_lcl_grps_n;		/* used number of v_lcl_grps */
-	u_long	v_cached_group;		/* last grp looked-up (phyints only) */
-	int	v_cached_result;	/* last look-up result (phyints only) */
+	u_char v_flags;            ///< VIFF_ flags defined above
+	u_char v_threshold;        ///< min ttl required to forward on vif
+	struct in_addr v_lcl_addr; ///< local interface address
+	struct in_addr v_rmt_addr; ///< remote address (tunnels only)
+	struct ifnet *v_ifp;       ///< pointer to interface
+	struct in_addr *v_lcl_grps;///< list of local grps (phyints only)
+	int v_lcl_grps_max;        ///< malloc'ed number of v_lcl_grps
+	int v_lcl_grps_n;          ///< used number of v_lcl_grps
+	u_long v_cached_group;     ///< last grp looked-up (phyints only)
+	int v_cached_result;       ///< last look-up result (phyints only)
 };
 
-/*
+/**
  * The kernel's multicast route structure.
  */
 struct mrt {
-	struct	in_addr mrt_origin;	/* subnet origin of multicasts */
-	struct	in_addr mrt_originmask;	/* subnet mask for origin */
-	vifi_t	mrt_parent;    		/* incoming vif */
-	vifbitmap_t mrt_children;	/* outgoing children vifs */
-	vifbitmap_t mrt_leaves;		/* subset of outgoing children vifs */
-	struct	mrt *mrt_next;		/* forward link */
+	struct in_addr mrt_origin;    ///< subnet origin of multicasts
+	struct in_addr mrt_originmask;///< subnet mask for origin
+	vifi_t mrt_parent;            ///< incoming vif
+	vifbitmap_t mrt_children;     ///< outgoing children vifs
+	vifbitmap_t mrt_leaves;       ///< subset of outgoing children vifs
+	struct mrt *mrt_next;         ///< forward link
 };
 
 
-#define	MRTHASHSIZ	64
-#if (MRTHASHSIZ & (MRTHASHSIZ - 1)) == 0	  /* from sys:route.h */
-#define	MRTHASHMOD(h)	((h) & (MRTHASHSIZ - 1))
+#define MRTHASHSIZ 64
+#if(MRTHASHSIZ & (MRTHASHSIZ - 1)) == 0 /* from sys:route.h */
+#define MRTHASHMOD(h) ((h) & (MRTHASHSIZ - 1))
 #else
-#define	MRTHASHMOD(h)	((h) % MRTHASHSIZ)
+#define MRTHASHMOD(h) ((h) % MRTHASHSIZ)
 #endif
 
-/*
+/**
  * The kernel's multicast routing statistics.
  */
 struct mrtstat {
-	u_long	mrts_mrt_lookups;	/* # multicast route lookups */
-	u_long	mrts_mrt_misses;	/* # multicast route cache misses */
-	u_long	mrts_grp_lookups;	/* # group address lookups */
-	u_long	mrts_grp_misses;	/* # group address cache misses */
-	u_long	mrts_no_route;		/* no route for packet's origin */
-	u_long	mrts_bad_tunnel;	/* malformed tunnel options */
-	u_long	mrts_cant_tunnel;	/* no room for tunnel options */
-	u_long	mrts_wrong_if;		/* arrived on the wrong interface */
+	u_long mrts_mrt_lookups;///< 查找的多播路由数 # multicast route lookups
+	u_long mrts_mrt_misses; ///< 高速缓存丢失的多播路由数 # multicast route cache misses
+	u_long mrts_grp_lookups;///< 查找的组地址数 # group address lookups
+	u_long mrts_grp_misses; ///< 高速缓存丢失的组地址数 # group address cache misses
+	u_long mrts_no_route;   ///< 查找失败的多播路由数 no route for packet's origin
+	u_long mrts_bad_tunnel; ///< 有错误的隧道选项的分组数 malformed tunnel options
+	u_long mrts_cant_tunnel;///< 没有空间存放隧道选项的分组数 no room for tunnel options
+	u_long mrts_wrong_if;   ///< arrived on the wrong interface
 };
 
 
-int	ip_mrouter_cmd __P((int, struct socket *, struct mbuf *));
-int	ip_mrouter_done __P((void));
+int ip_mrouter_cmd __P((int, struct socket *, struct mbuf *) );
+int ip_mrouter_done __P((void) );
 
 #endif /* KERNEL */
