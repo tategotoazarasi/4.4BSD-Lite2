e5fbbf2225a9a1905d878421f2328d5527bf532d
Tategoto Azarasi <2724167997@qq.com>
2023-08-30 17:42:11 +0800

    BPF:BSD分组过滤程序
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- sys/net/bpf.h
+++ sys/net/bpf.h
@@ -1,4 +1,6 @@
-/*
+/**
+ * @file
+ * @copyright
  * Copyright (c) 1990, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -45,29 +47,29 @@
  * even multiple of BPF_ALIGNMENT. 
  */
 #define BPF_ALIGNMENT sizeof(long)
-#define BPF_WORDALIGN(x) (((x)+(BPF_ALIGNMENT-1))&~(BPF_ALIGNMENT-1))
+#define BPF_WORDALIGN(x) (((x) + (BPF_ALIGNMENT - 1)) & ~(BPF_ALIGNMENT - 1))
 
 #define BPF_MAXINSNS 512
 #define BPF_MAXBUFSIZE 0x8000
 #define BPF_MINBUFSIZE 32
 
-/*
+/**
  *  Structure for BIOCSETF.
  */
 struct bpf_program {
 	u_int bf_len;
 	struct bpf_insn *bf_insns;
 };
- 
-/*
+
+/**
  * Struct returned by BIOCGSTATS.
  */
 struct bpf_stat {
-	u_int bs_recv;		/* number of packets received */
-	u_int bs_drop;		/* number of packets dropped */
+	u_int bs_recv;///< number of packets received
+	u_int bs_drop;///< number of packets dropped
 };
 
-/*
+/**
  * Struct return by BIOCVERSION.  This represents the version number of 
  * the filter language described by the instruction encodings below.
  * bpf understands a program iff kernel_major == filter_major &&
@@ -93,45 +95,44 @@ struct bpf_version {
  * header files.  If your using gcc, we assume that you
  * have run fixincludes so the latter set should work.
  */
-#if (defined(sun) || defined(ibm032)) && !defined(__GNUC__)
-#define	BIOCGBLEN	_IOR(B,102, u_int)
-#define	BIOCSBLEN	_IOWR(B,102, u_int)
-#define	BIOCSETF	_IOW(B,103, struct bpf_program)
-#define	BIOCFLUSH	_IO(B,104)
-#define BIOCPROMISC	_IO(B,105)
-#define	BIOCGDLT	_IOR(B,106, u_int)
-#define BIOCGETIF	_IOR(B,107, struct ifreq)
-#define BIOCSETIF	_IOW(B,108, struct ifreq)
-#define BIOCSRTIMEOUT	_IOW(B,109, struct timeval)
-#define BIOCGRTIMEOUT	_IOR(B,110, struct timeval)
-#define BIOCGSTATS	_IOR(B,111, struct bpf_stat)
-#define BIOCIMMEDIATE	_IOW(B,112, u_int)
-#define BIOCVERSION	_IOR(B,113, struct bpf_version)
+#if(defined(sun) || defined(ibm032)) && !defined(__GNUC__)
+#define BIOCGBLEN _IOR(B, 102, u_int)
+#define BIOCSBLEN _IOWR(B, 102, u_int)
+#define BIOCSETF _IOW(B, 103, struct bpf_program)
+#define BIOCFLUSH _IO(B, 104)
+#define BIOCPROMISC _IO(B, 105)
+#define BIOCGDLT _IOR(B, 106, u_int)
+#define BIOCGETIF _IOR(B, 107, struct ifreq)
+#define BIOCSETIF _IOW(B, 108, struct ifreq)
+#define BIOCSRTIMEOUT _IOW(B, 109, struct timeval)
+#define BIOCGRTIMEOUT _IOR(B, 110, struct timeval)
+#define BIOCGSTATS _IOR(B, 111, struct bpf_stat)
+#define BIOCIMMEDIATE _IOW(B, 112, u_int)
+#define BIOCVERSION _IOR(B, 113, struct bpf_version)
 #else
-#define	BIOCGBLEN	_IOR('B',102, u_int)
-#define	BIOCSBLEN	_IOWR('B',102, u_int)
-#define	BIOCSETF	_IOW('B',103, struct bpf_program)
-#define	BIOCFLUSH	_IO('B',104)
-#define BIOCPROMISC	_IO('B',105)
-#define	BIOCGDLT	_IOR('B',106, u_int)
-#define BIOCGETIF	_IOR('B',107, struct ifreq)
-#define BIOCSETIF	_IOW('B',108, struct ifreq)
-#define BIOCSRTIMEOUT	_IOW('B',109, struct timeval)
-#define BIOCGRTIMEOUT	_IOR('B',110, struct timeval)
-#define BIOCGSTATS	_IOR('B',111, struct bpf_stat)
-#define BIOCIMMEDIATE	_IOW('B',112, u_int)
-#define BIOCVERSION	_IOR('B',113, struct bpf_version)
+#define BIOCGBLEN _IOR('B', 102, u_int)               ///< 返回分组缓存大小
+#define BIOCSBLEN _IOWR('B', 102, u_int)              ///< 设定分组缓存大小
+#define BIOCSETF _IOW('B', 103, struct bpf_program)   ///< 安装BPF程序
+#define BIOCFLUSH _IO('B', 104)                       ///< 丢弃挂起分组
+#define BIOCPROMISC _IO('B', 105)                     ///< 设定混杂方式
+#define BIOCGDLT _IOR('B', 106, u_int)                ///< 返回bif_dlt
+#define BIOCGETIF _IOR('B', 107, struct ifreq)        ///< 返回所属接口的名称
+#define BIOCSETIF _IOW('B', 108, struct ifreq)        ///< 为网络接口添加设备
+#define BIOCSRTIMEOUT _IOW('B', 109, struct timeval)  ///< 设定“读”操作的超时时限
+#define BIOCGRTIMEOUT _IOR('B', 110, struct timeval)  ///< 返回“读”操作的超时时限
+#define BIOCGSTATS _IOR('B', 111, struct bpf_stat)    ///< 返回BPF统计值
+#define BIOCIMMEDIATE _IOW('B', 112, u_int)           ///< 设定立即方式
+#define BIOCVERSION _IOR('B', 113, struct bpf_version)///< 返回BPF版本信息
 #endif
 
-/*
+/**
  * Structure prepended to each packet.
  */
 struct bpf_hdr {
-	struct timeval	bh_tstamp;	/* time stamp */
-	u_long		bh_caplen;	/* length of captured portion */
-	u_long		bh_datalen;	/* original length of packet */
-	u_short		bh_hdrlen;	/* length of bpf header (this struct
-					   plus alignment padding) */
+	struct timeval bh_tstamp;///< time stamp
+	u_long bh_caplen;        ///< length of captured portion
+	u_long bh_datalen;       ///< original length of packet
+	u_short bh_hdrlen;       ///< length of bpf header (this struct plus alignment padding)
 };
 /*
  * Because the structure above is not a multiple of 4 bytes, some compilers
@@ -146,107 +147,105 @@ struct bpf_hdr {
  * Data-link level type codes.
  * Currently, only DLT_EN10MB and DLT_SLIP are supported.
  */
-#define DLT_NULL	0	/* no link-layer encapsulation */
-#define DLT_EN10MB	1	/* Ethernet (10Mb) */
-#define DLT_EN3MB	2	/* Experimental Ethernet (3Mb) */
-#define DLT_AX25	3	/* Amateur Radio AX.25 */
-#define DLT_PRONET	4	/* Proteon ProNET Token Ring */
-#define DLT_CHAOS	5	/* Chaos */
-#define DLT_IEEE802	6	/* IEEE 802 Networks */
-#define DLT_ARCNET	7	/* ARCNET */
-#define DLT_SLIP	8	/* Serial Line IP */
-#define DLT_PPP		9	/* Point-to-point Protocol */
-#define DLT_FDDI	10	/* FDDI */
+#define DLT_NULL 0   ///< 环回接口 no link-layer encapsulation
+#define DLT_EN10MB 1 ///< 10 Mb以太网接口 Ethernet (10Mb)
+#define DLT_EN3MB 2  ///< Experimental Ethernet (3Mb)
+#define DLT_AX25 3   ///< Amateur Radio AX.25
+#define DLT_PRONET 4 ///< Proteon ProNET Token Ring
+#define DLT_CHAOS 5  ///< Chaos
+#define DLT_IEEE802 6///< IEEE 802 Networks
+#define DLT_ARCNET 7 ///< ARCNET
+#define DLT_SLIP 8   ///< SLIP接口 Serial Line IP
+#define DLT_PPP 9    ///< Point-to-point Protocol
+#define DLT_FDDI 10  ///< FDDI
 
 /*
  * The instruction encondings.
  */
 /* instruction classes */
-#define BPF_CLASS(code) ((code) & 0x07)
-#define		BPF_LD		0x00
-#define		BPF_LDX		0x01
-#define		BPF_ST		0x02
-#define		BPF_STX		0x03
-#define		BPF_ALU		0x04
-#define		BPF_JMP		0x05
-#define		BPF_RET		0x06
-#define		BPF_MISC	0x07
+#define BPF_CLASS(code) ((code) &0x07)
+#define BPF_LD 0x00
+#define BPF_LDX 0x01
+#define BPF_ST 0x02
+#define BPF_STX 0x03
+#define BPF_ALU 0x04
+#define BPF_JMP 0x05
+#define BPF_RET 0x06
+#define BPF_MISC 0x07
 
 /* ld/ldx fields */
-#define BPF_SIZE(code)	((code) & 0x18)
-#define		BPF_W		0x00
-#define		BPF_H		0x08
-#define		BPF_B		0x10
-#define BPF_MODE(code)	((code) & 0xe0)
-#define		BPF_IMM 	0x00
-#define		BPF_ABS		0x20
-#define		BPF_IND		0x40
-#define		BPF_MEM		0x60
-#define		BPF_LEN		0x80
-#define		BPF_MSH		0xa0
+#define BPF_SIZE(code) ((code) &0x18)
+#define BPF_W 0x00
+#define BPF_H 0x08
+#define BPF_B 0x10
+#define BPF_MODE(code) ((code) &0xe0)
+#define BPF_IMM 0x00
+#define BPF_ABS 0x20
+#define BPF_IND 0x40
+#define BPF_MEM 0x60
+#define BPF_LEN 0x80
+#define BPF_MSH 0xa0
 
 /* alu/jmp fields */
-#define BPF_OP(code)	((code) & 0xf0)
-#define		BPF_ADD		0x00
-#define		BPF_SUB		0x10
-#define		BPF_MUL		0x20
-#define		BPF_DIV		0x30
-#define		BPF_OR		0x40
-#define		BPF_AND		0x50
-#define		BPF_LSH		0x60
-#define		BPF_RSH		0x70
-#define		BPF_NEG		0x80
-#define		BPF_JA		0x00
-#define		BPF_JEQ		0x10
-#define		BPF_JGT		0x20
-#define		BPF_JGE		0x30
-#define		BPF_JSET	0x40
-#define BPF_SRC(code)	((code) & 0x08)
-#define		BPF_K		0x00
-#define		BPF_X		0x08
+#define BPF_OP(code) ((code) &0xf0)
+#define BPF_ADD 0x00
+#define BPF_SUB 0x10
+#define BPF_MUL 0x20
+#define BPF_DIV 0x30
+#define BPF_OR 0x40
+#define BPF_AND 0x50
+#define BPF_LSH 0x60
+#define BPF_RSH 0x70
+#define BPF_NEG 0x80
+#define BPF_JA 0x00
+#define BPF_JEQ 0x10
+#define BPF_JGT 0x20
+#define BPF_JGE 0x30
+#define BPF_JSET 0x40
+#define BPF_SRC(code) ((code) &0x08)
+#define BPF_K 0x00
+#define BPF_X 0x08
 
 /* ret - BPF_K and BPF_X also apply */
-#define BPF_RVAL(code)	((code) & 0x18)
-#define		BPF_A		0x10
+#define BPF_RVAL(code) ((code) &0x18)
+#define BPF_A 0x10
 
 /* misc */
-#define BPF_MISCOP(code) ((code) & 0xf8)
-#define		BPF_TAX		0x00
-#define		BPF_TXA		0x80
+#define BPF_MISCOP(code) ((code) &0xf8)
+#define BPF_TAX 0x00
+#define BPF_TXA 0x80
 
-/*
+/**
  * The instruction data structure.
  */
 struct bpf_insn {
-	u_short	code;
-	u_char 	jt;
-	u_char 	jf;
-	long	k;
+	u_short code;
+	u_char jt;
+	u_char jf;
+	long k;
 };
 
 /*
  * Macros for insn array initializers.
  */
-#define BPF_STMT(code, k) { (u_short)(code), 0, 0, k }
-#define BPF_JUMP(code, k, jt, jf) { (u_short)(code), jt, jf, k }
+#define BPF_STMT(code, k) \
+	{ (u_short)(code), 0, 0, k }
+#define BPF_JUMP(code, k, jt, jf) \
+	{ (u_short)(code), jt, jf, k }
 
 #ifdef KERNEL
-int	 bpf_validate __P((struct bpf_insn *, int));
-int	 bpfopen __P((dev_t, int));
-int	 bpfclose __P((dev_t, int));
-int	 bpfread __P((dev_t, struct uio *));
-int	 bpfwrite __P((dev_t, struct uio *));
-int	 bpfioctl __P((dev_t, u_long, caddr_t, int));
-int	 bpf_select __P((dev_t, int, struct proc *));
-void	 bpf_tap __P((caddr_t, u_char *, u_int));
-void	 bpf_mtap __P((caddr_t, struct mbuf *));
-void	 bpfattach __P((caddr_t *, struct ifnet *, u_int, u_int));
-void	 bpfilterattach __P((int));
-u_int	 bpf_filter __P((struct bpf_insn *, u_char *, u_int, u_int));
+int bpf_validate __P((struct bpf_insn *, int) );
+int bpfopen __P((dev_t, int) );
+int bpfclose __P((dev_t, int) );
+int bpfread __P((dev_t, struct uio *) );
+int bpfwrite __P((dev_t, struct uio *) );
+int bpfioctl __P((dev_t, u_long, caddr_t, int) );
+int bpf_select __P((dev_t, int, struct proc *) );
+void bpf_tap __P((caddr_t, u_char *, u_int));
+void bpf_mtap __P((caddr_t, struct mbuf *) );
+void bpfattach __P((caddr_t *, struct ifnet *, u_int, u_int));
+void bpfilterattach __P((int) );
+u_int bpf_filter __P((struct bpf_insn *, u_char *, u_int, u_int));
 #endif
 
-/*
- * Number of scratch memory words (for BPF_LD|BPF_MEM and BPF_ST).
- */
-#define BPF_MEMWORDS 16
-
+#define BPF_MEMWORDS 16///< Number of scratch memory words (for BPF_LD|BPF_MEM and BPF_ST).
